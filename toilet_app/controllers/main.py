c6bb8a499fb88fba72c57af9b59c122ae0e299c6
Renaldi <renaldi@boku.com>
2014-06-25 15:56:35 -0700

    persist toilet status

--- toilet_app/controllers/main.py
+++ toilet_app/controllers/main.py
@@ -6,6 +6,9 @@ from toilet_app import app, socketio
 MAIN_TOILET = 1
 SECOND_TOILET = 2
 
+MAIN_TOILET_STATUS = "available"
+SECOND_TOILET_STATUS = "available"
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -21,11 +24,13 @@ def status_update(toilet_id):
         status = request.args.get('status')
 
     if MAIN_TOILET == toilet_id:
+        MAIN_TOILET_STATUS = status
         socketio.emit('main toilet status',
                       {'data': status},
                       namespace='/test')
 
     elif SECOND_TOILET == toilet_id:
+        SECOND_TOILET_STATUS = status
         socketio.emit('second toilet status',
                       {'data': status},
                       namespace='/test')
@@ -35,10 +40,10 @@ def status_update(toilet_id):
 @socketio.on('connect', namespace='/test')
 def test_connect():
     socketio.emit('main toilet status',
-                      {'data': 'available'},
+                      {'data': MAIN_TOILET_STATUS},
                       namespace='/test')
     socketio.emit('second toilet status',
-              {'data': 'available'},
+              {'data': SECOND_TOILET_STATUS},
               namespace='/test')
 
 @socketio.on('disconnect', namespace='/test')
